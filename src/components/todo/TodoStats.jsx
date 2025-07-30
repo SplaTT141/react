fc2ea659fa6978b0211edc4287bf05c3fd9b8ae6
Ernest <zyperis14@gmail.com>
2025-07-30 09:57:48 +0300

    todo list edittting

--- src/components/todo/TodoStats.jsx
+++ src/components/todo/TodoStats.jsx
@@ -1,9 +1,9 @@
 export function TodoStats({ activeCount, completedCount }) {
     return (
         <>
-            <h2>Task count: {activeCount + completedCount}</h2>
-            <p>Active tasks: {activeCount}</p>
             <p>Completed tasks: {completedCount}</p>
+            <p>Active tasks: {activeCount}</p>
+            <h2>Task count: {activeCount + completedCount}</h2>
         </>
     )
 }
